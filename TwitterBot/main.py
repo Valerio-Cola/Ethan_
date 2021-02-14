eab9bff93bcb98f288621153f29825ded77ba9ea
Valerio Cola <62888688+Valerio-Cola@users.noreply.github.com>
2021-02-14 23:56:45 +0100

    Update main.py

--- TwitterBot/main.py
+++ TwitterBot/main.py
@@ -5,10 +5,10 @@ import requests
 import tweepy 
 import time
 
-consumer_key = 'cJGILwxzmlcr59Jbkwta9O0nM' 
-consumer_secret = 'FYplFtMriP5rxOySgnBMfM4rAlzlST9v19fGzVF3RoeGULgGSe' 
-access_token = '1356642176021708802-6m6Y1W4SE4tTv8K4i0pS8vYILCXNYH' 
-access_token_secret = 'VSloJBx85qUKBTuBAPhaKnB0VMouQEpZhz2GBBnrzsOCr' 
+consumer_key = 'ma4i41VNEylPopbEWMEQN7dvj' 
+consumer_secret = 'fRYpXYXfRBtYXwJcNhJ9tCX2qcspXdDjCVuY9Wx08IbPTdcw99' 
+access_token = '1361078733902381057-yjxQBL3NMbQ99QVrQMyzuA5mOuEPDk' 
+access_token_secret = 'eLCtpOklgL4GeVfqrPhtQlVbtwhcHz0gWTJfvvZAgyUSX' 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret) 
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
