0f247a84ef0ab69c120db433001af2985124714e
Valerio Cola <62888688+Valerio-Cola@users.noreply.github.com>
2022-11-22 20:55:08 +0100

    Update getLink.py

--- getLink.py
+++ getLink.py
@@ -1,15 +1,17 @@
 ###---Get link for the artwork with selenium, using bs4 it doesn't work because the link is incorporated into a ViewModel---###
-
+from selenium.webdriver.common.by import By
 from selenium import webdriver
 
 truelink = []
 links = []
 
+
+
 def searcher():
     driver = webdriver.Chrome()
     driver.get('https://www.wikiart.org/')
-    for a in driver.find_elements_by_tag_name('a'):
+    b = driver.find_elements(By.XPATH, "/html/body/div[2]/div[1]/section/main/div[2]/article/ul/li[2]/a")
+    for a in b:
         links.append(a.get_attribute('href'))
-    truelink.append(links[65])
+    truelink.append(links[0])
     links.clear()
-
