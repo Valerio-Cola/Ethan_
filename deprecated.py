4e88a6e7336b005cae236b681388e0cb0ec0c29b
Valerio Cola <62888688+Valerio-Cola@users.noreply.github.com>
2024-10-24 23:35:45 +0200

    Add files via upload

---
+++ deprecated.py
@@ -0,0 +1,148 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import requests
+
+from deprecated2 import searcher, truelink
+import random
+from github import Github
+
+##cFpHd05WaGhoNW1lYlMyU3U4RVQ6MTpjaQ zSB8jgFacOxeNzp1hFJARRwDFC2UyGSDRjVuKroWZfMKrxKJmk 
+###---Login into TwitterAPI---###
+from tweepy import api
+import tweepy 
+
+consumer_key = 'QpZSniG92kP23NFvtMdlkn0s8' 
+consumer_secret = 'nOljbT3D4WiG0cbZhsXKxMo4wXkXyt5PmcwB4kK07dNC9WQ9TJ' 
+access_token = '1595096122472763400-9tJnGFPeqyNMlXvlqiP2ogRq7iiFYk' 
+access_token_secret = 'dm00LwgKOK4zBu8HpqzbiLGiXn1PIBFknOcurEharU2Xz' 
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret) 
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+
+
+
+###---Get Informations about the link---###
+
+informations = []
+
+def infogetter():
+
+    # Get title
+    titles = soup2.find("h1") 
+    informations.append("\U0001f5bc\uFE0F" + titles.text)
+    titolo = titles.text
+    # Get author
+    author = soup2.find("h2")   
+    informations.append(author.text)
+    autore = author.text
+    # Get image
+    img = soup2.find(itemprop= "image") 
+    image1 = img.get("src")
+    response = requests.get(image1)
+    file = open("sample_image.png", "wb")
+    file.write(response.content)
+    file.close()
+    
+    # Get date
+    for z in soup2.find_all("span", attrs={"itemprop":"dateCreated"}): 
+        z1 = z.get_text()
+        informations.append(z1)
+    
+    # Get generic informations
+    for x in soup2.find_all("li", attrs={"class":"dictionary-values"}): 
+        x1 = x.text
+        x2 = x1.replace("\n", "")
+        x3 = x2[:6]
+        informations.append(x3 + " " + x2[6:])
+
+    # Get gallery location
+    for y in soup2.find_all("li", attrs={"class":"dictionary-values-gallery"}): 
+        y1 = y.get_text()
+        y2 = y1.replace("\n", "")
+        y3 = y2.replace("Location:" , "")
+        informations.append("\U0001f4cd Location: " + y3)
+
+    print(informations)
+    database(titolo, autore)
+    
+
+
+
+###---Search if the same tweet already exist---###
+
+def duplicate_finder(titles, author):
+
+    duplicated = titles.text + ", " + author.text
+    lista_query = []
+    tweets = api.user_timeline(screen_name = "Ethan Algorithm", count = 50, include_rts = False, tweet_mode = 'extended')
+
+    for info in tweets:
+         appended = info.full_text
+         appended1 = appended.replace("\n","")
+         index = appended1.find("Style")
+         lista_query.append(appended1[:int(index)])
+
+    if duplicated in lista_query:
+        return
+    else:
+        tweeter()
+
+
+###---Publish the tweet---###
+
+hash_art = ["#artist", "#artnews", "#artinfo", "#painting", "#paint", "#art", "#drawing", "#colors", "#artwork", "#arte", "#artistic"]
+
+def tweeter():
+    has_name = informations[1].replace(" " ,"")
+    last_hash = random.choice(hash_art)
+    
+
+    message = ', '.join(map(str, informations[:2])) + f" {informations[2]}\n\n" + "\U0001f3a8" + '\n'.join(map(str, informations[3:])) + f"\n\n #{has_name} #art #artgallery {last_hash}\n Source: {truelink[0]}"
+    
+    media = api.media_upload("sample_image.png")
+    api.update_status(status=message, media_ids=[media.media_id])
+
+
+
+
+def database(titolo, autore):
+
+    g = Github("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")
+    repo = g.get_user().get_repo('EthanStorage')
+    # update
+    file = repo.get_contents("db.txt", ref="main")
+    vecchiofile = file.decoded_content.decode()
+
+    new_data = f"{titolo} {autore}"
+    
+    if new_data not in vecchiofile:
+        repo.update_file(file.path, "", f"{vecchiofile}  \n{new_data}", file.sha, branch="main")
+        tweeter()
+    else:
+        return
+        
+
+
+###---Call the function of getLink.py---###
+
+searcher()
+
+
+
+###---Enter into the link with bs4 obtained with getLink.py---###
+
+url2 = truelink[0]
+page2 = urlopen(url2)
+html2 = page2.read().decode("utf-8")
+soup2 = BeautifulSoup(html2, "html.parser")
+
+
+
+###---Get Informations about the link---###
+
+infogetter()
+
+
+
---
+++ main.py
@@ -0,0 +1,168 @@
+import random
+import tweepy
+from tweepy import api
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+import requests
+
+from github import Github
+
+import os
+from dotenv import load_dotenv
+
+# Carica le variabili di ambiente dal file .env se esiste
+load_dotenv()
+
+
+# Le chiavi e i token di accesso di Twitter
+CONSUMER_KEY = os.getenv('CONSUMER_KEY')
+CONSUMER_SECRET = os.getenv('CONSUMER_SECRET')
+ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
+ACCESS_TOKEN_SECRET = os.getenv('ACCESS_TOKEN_SECRET')
+GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
+
+def create_client():
+    
+    auth = tweepy.OAuth1UserHandler(
+        consumer_key=CONSUMER_KEY,
+        consumer_secret=CONSUMER_SECRET,
+        access_token=ACCESS_TOKEN,
+        access_token_secret=ACCESS_TOKEN_SECRET
+    )
+
+    api = tweepy.API(auth)
+    
+    try:
+        api.verify_credentials()
+        print("Autenticazione avvenuta con successo")
+    except tweepy.TweepyException as e:
+        print("Errore durante l'autenticazione", e)
+    
+    client = tweepy.Client(
+        consumer_key=CONSUMER_KEY,
+        consumer_secret=CONSUMER_SECRET,
+        access_token=ACCESS_TOKEN,
+        access_token_secret=ACCESS_TOKEN_SECRET
+    )
+    
+    return client, api
+
+def tweet(client, api, message, image_path):
+    media = api.media_upload(image_path)
+    client.create_tweet(text=message, media_ids=[media.media_id])
+    print("Tweet inviato: " + message)
+
+def database(titolo, autore):
+
+    g = Github(GITHUB_TOKEN)
+    repo = g.get_user().get_repo('EthanStorage')
+    
+    # update
+    file = repo.get_contents("db2.txt", ref="main")
+    vecchiofile = file.decoded_content.decode()
+
+    new_data = f"{titolo} {autore}"
+    
+    if new_data not in vecchiofile:
+        repo.update_file(file.path, "", f"{vecchiofile}  \n{new_data}", file.sha, branch="main")
+        return 0
+    else:
+        return 1
+    
+
+def main():
+
+
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    driver.get('https://www.wikiart.org/')
+
+    # Trova gli elementi dei link usando XPath
+    link = driver.find_elements(By.XPATH, '/html/body/div[2]/div[1]/section/main/div[2]/article/h3/a')
+
+
+    truelink = link[0].get_attribute('href')
+
+    # Clicca sul link
+    link[0].click()
+
+    # Trova l'elemento <article>
+    article = driver.find_element(By.TAG_NAME, 'article')
+
+    h3_element = article.find_element(By.TAG_NAME, 'h3')
+    print("H3 Element: ", h3_element.text)
+    msg = f"\U0001f5bc {h3_element.text}"
+
+    # Trova l'elemento <h5> all'interno di <article>
+    h5_element = article.find_element(By.TAG_NAME, 'h5')
+    print("H5 Element: ", h5_element.text)
+    msg += f",{h5_element.text}"
+
+    msg += "\n\n"
+    try:
+        ul_element = article.find_element(By.TAG_NAME, 'ul')
+        li_elements = ul_element.find_element(By.CLASS_NAME, 'dictionary-values-gallery')
+
+        s_element = li_elements.find_element(By.TAG_NAME, 's').text
+        span_element = li_elements.find_element(By.TAG_NAME, 'span').text
+        print(s_element+ " " + span_element)
+        print()
+        msg += f"\U0001F3DB {s_element} {span_element}\n"
+
+    except:
+        print('NO LUOGO')
+    
+
+    li_elements = ul_element.find_elements(By.CLASS_NAME, 'dictionary-values')
+
+    # Itera sugli elementi <li> e stampa il contenuto dei tag <s> e <span>
+    for li in li_elements:
+        s_element = li.find_element(By.TAG_NAME, 's').text
+        span_element = li.find_element(By.TAG_NAME, 'span').text
+        print(s_element)
+        print(span_element)
+        if s_element == "Style:":
+            msg += "\U0001f3a8"
+        elif s_element == "Media:":
+            msg += "\U0001F58C"
+
+        msg += f"{s_element} {span_element}\n"
+
+    # Trova l'elemento dell'immagine con itemprop="image"
+    image_element = driver.find_element(By.XPATH, '//img[@itemprop="image"]')
+    image_url = image_element.get_attribute('src')
+
+    # Scarica l'immagine
+    image_data = requests.get(image_url).content
+    with open('quadro.jpg', 'wb') as file:
+        file.write(image_data)
+    file.close()
+    
+    hash_art = ["#artist", "#artnews", "#artinfo", "#painting", "#paint", "#art", "#drawing", "#colors", "#artwork", "#arte", "#artistic"]
+    
+    
+    msg += f"\n#{h5_element.text.replace(' ', '')} #art #artgallery {random.choice(hash_art)}\n\U0001F517 {truelink}"
+
+    if database(h3_element.text, h5_element.text) == 1:
+        return
+    else:
+        print(msg)
+        client, api = create_client()
+        tweet(client, api, msg, 'quadro.jpg')
+
+    # Chiudi il driver
+    driver.quit()
+
+
+
+
+    
+if __name__ == "__main__":
+    main()
